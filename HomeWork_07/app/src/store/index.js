ca4869bd917adacec84a6d94fcbaf05f40a51fcd
Dmitriy <dimakravchenko1989@yandex.ru>
2024-03-18 02:21:07 +0300

    Добавил папку ДЗ-7

--- HomeWork_07/app/src/store/index.js
+++ HomeWork_07/app/src/store/index.js
@@ -1,8 +1,6 @@
 import { createStore } from "vuex";
 
-export default createStore
-// export default new Vuex.Store
-    ({
+export default createStore ({
         state: {
             header: 'Minimal Look Bedrooms',
             text_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aliquamsem vitae turpis dignissim maximus. Aliquam sollicitudin tellumassa, vbel maximus purus posuere in. Dojrices gravida dignissim. Praesent at nibh in mi fringilla mattis. Phasellus ut dolor odio. Aenean in the ipsum vel lectus bibendum commodo.',
